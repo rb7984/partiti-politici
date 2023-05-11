79cecffc05b3e814c6a2d695c5065c6b0a8d0c70
riccbarelli <99691825+rb7984@users.noreply.github.com>
2023-05-11 14:44:25 +0200

    first steps

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/PD2023.png'
+import logo2 from './images/FDI2023.png'
 
 function App() {
   return (
     <div className="App">
       <div className ="partito">
-        PD
+      <img 
+          alt="PD NOT FOUND" 
+          src={logo}
+          className='logoPartito'
+        /> 
       </div>
       <div className ="partito">
-        FDI
+      <img 
+          alt="FDI NOT FOUND" 
+          src={logo2}
+          className='logoPartito'
+        /> 
       </div>
     </div>
   );
